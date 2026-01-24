9a0b01bdc56de5c1f5f7c2f694b1ef17d7ccda70
nirmal <nirmal.elex@gmail.com>
2026-01-24 22:25:11 +0530

    final code

--- devopdcicdproject/src/main/java/com/devops/App.java
+++ devopdcicdproject/src/main/java/com/devops/App.java
@@ -1,7 +1,24 @@
 package com.devops;
 
+import com.sun.net.httpserver.HttpServer;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
+
 public class App {
-    public static void main(String[] args) {
-        System.out.println("Hello DevOps CI/CD Project & Nirmal Shanker ");
+    public static void main(String[] args) throws Exception {
+
+        HttpServer server = HttpServer.create(new InetSocketAddress(8000), 0);
+
+        server.createContext("/", exchange -> {
+            String response = "Hello DevOps CI/CD Project & Nirmal Shanker";
+            exchange.sendResponseHeaders(200, response.length());
+            OutputStream os = exchange.getResponseBody();
+            os.write(response.getBytes());
+            os.close();
+        });
+
+        server.start();
+        System.out.println("Web server started on port 8000");
     }
 }
+
